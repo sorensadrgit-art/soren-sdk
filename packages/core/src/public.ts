fe4fee607b638b907a9dfacf80f90299118cdcf9
sorensadrgit-art <sorensadr.git@gmail.com>
2026-07-31 04:44:18 -0400

    chore: add core public export aggregator

---
+++ packages/core/src/public.ts
@@ -0,0 +1,7 @@
+export * from "./catalog/service.js";
+export * from "./catalog/types.js";
+export * from "./inspector/types.js";
+export * from "./inspector/inspect-project.js";
+export * from "./router/policy.js";
+export * from "./router/route-capabilities-workspace-reuse.js";
+export * from "./router/types.js";
